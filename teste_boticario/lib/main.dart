3ed24a3d92ff691eb50bccc785b5768aa2b0dc0a
DanielGunna <danielgunna@ioasys.com.br>
2021-01-11 23:34:06 -0300

    disable debub banner
    disable debub banner

--- teste_boticario/lib/main.dart
+++ teste_boticario/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Teste boticario',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
